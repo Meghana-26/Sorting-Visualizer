cd81418a951af9519b4ed42f6c270edd6a102a61
Meghana Pandraju <meghanasrimanvitha@gmail.com>
2023-06-23 15:43:37 +0530

    Add files via upload

---
+++ Sorting_Visualizer/scripts/merge_sort.js
@@ -0,0 +1,63 @@
+function merge(left, right) {
+    let sortedArr = []; // the sorted elements will go here
+  
+    while (left.length && right.length) {
+      // insert the smallest element to the sortedArr
+      if (left[0] < right[0]) {
+        sortedArr.push(left.shift());
+      } else {
+        sortedArr.push(right.shift());
+      }
+    }
+  }
+
+function merge(left, right) {
+  let sortedArr = []; // the sorted elements will go here
+
+  while (left.length && right.length) {
+    // insert the smallest element to the sortedArr
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+
+  console.log(sortedArr); // [2, 3, 4, 5]
+  console.log(left); // [7]
+  console.log(right); // []
+}
+
+function merge(left, right) {
+  let sortedArr = []; // the sorted elements will go here
+
+  while (left.length && right.length) {
+    // insert the smallest element to the sortedArr
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+
+  // use spread operator and create a new array, combining the three arrays
+  return [...sortedArr, ...left, ...right];
+}
+// With that, you have the merge() function for the merge sort ready. Now you only need a function that splits an array into 1 size.
+
+// Here’s an example of the mergeSort() function:
+
+function mergeSort(arr) {
+  const half = arr.length / 2;
+
+  // the base case is array length <=1
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const left = arr.splice(0, half); 
+  const right = arr;
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+
